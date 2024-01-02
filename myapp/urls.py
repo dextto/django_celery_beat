d97855c7dda12506046949a8bedd3c2f83220215
dextto <dexter.haan@gmail.com>
2024-01-03 06:32:14 +0900

    refactor: move view to 'views' directory

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import index_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index_view.index, name='index'),
 ]
