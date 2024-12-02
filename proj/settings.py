3abe4ae7c90e28b1ee1b93c70f7b93b6dff3d594
dextto <dexter.haan@gmail.com>
2024-12-02 16:49:12 +0900

    fix: 모듈명 수정

--- proj/settings.py
+++ proj/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myapp",
-    # 'django_celery_beat',
+    "django_celery_beat",
     "django_celery_results",
 ]
 
